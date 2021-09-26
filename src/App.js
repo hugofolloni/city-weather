0ad9c5bb2a29a408868b03209217bf9a14ac8b05
hugofolloni <hugofollogua07@gmail.com>
2021-09-26 17:22:55 -0300

    :rocket: adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./globalStyles";
+import { nightTheme, dayTheme } from "./theme";
+import { useState } from "react"
+import "./App.css"
+import Header from "./Header";
+import Home from "./Home"
 
 function App() {
+
+  const [isDay, setIsDay] = useState(0)
+
+  const getIsDay = () => {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position => {
+          const long = position.coords.longitude;
+          const lat = position.coords.latitude;
+
+          var proxy = 'https://the-cors.herokuapp.com/';
+          var isdayapi = `${proxy}http://api.weatherapi.com/v1/current.json?key= 22370fad94b44b3c94300446211806&q=${lat},${long}&aqi=no`;
+
+          fetch(isdayapi)
+          .then((res) =>{
+            return res.json()
+          })
+          .then((data) =>{
+            console.log(data.current.is_day)
+            setIsDay(data.current.is_day)
+          })
+      })
+    }}
+
+  const [theme, setTheme] = useState('light');
+
+  const themeToggler = () => {
+    getIsDay()
+    isDay === 1 ? setTheme('dark') : setTheme('light')
+}
+
+  window.addEventListener('load', themeToggler)
+
   return (
+    <ThemeProvider theme={theme === 'dark' ? nightTheme : dayTheme}>
+    <GlobalStyles/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <Home />    
     </div>
+    </ThemeProvider>
   );
 }
 
